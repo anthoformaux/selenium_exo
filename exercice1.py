ec1663383e3184030e1c4131f998170192089cab
Anthony Formaux <antho.formaux@gmail.com>
2026-03-11 14:33:12 +0100

    1st commit - script selenium

---
+++ exercice1.py
@@ -0,0 +1,42 @@
+
+import time
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+
+def lancement_exemple():
+
+    try:
+        driver.get("https://example.com/")
+        time.sleep(1)
+
+
+        assert driver.title == "Example Domain", f"Titre incorrect : {driver.title}"
+        print(f"Titre vérifié : {driver.title}")
+
+        heading = driver.find_element(By.TAG_NAME, "body")
+
+        assert "Example Domain" in heading.text, "Texte non trouvé dans le titre"
+        print(f"Contenu vérifié : {heading.text}")
+
+        assert driver.current_url == "https://example.com/", f"URL incorrect : {driver.current_url}"
+        print(f"Url vérifié: {driver.current_url}")
+
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        driver.quit()
+
+##### dans ce script ####
+if __name__ == "__main__":
+    lancement_exemple()
+
+##### autre script#####
+import exercice1
+exercice1.lancement_exemple()
